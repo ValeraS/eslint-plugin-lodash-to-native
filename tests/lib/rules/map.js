fa430ef871db7af4d6fb57f9ddc95e0f6ac252db
ValeraS <ValeraS@users.noreply.github.com>
2019-09-25 02:44:16 +0300

    add map rule

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -1,24 +1,50 @@
 'use strict';
 
-const rule = require('../../../lib/rules/map');
+var rule = require('../../../lib/rules/map');
 
-const RuleTester = require('eslint').RuleTester;
+var RuleTester = require('eslint').RuleTester;
 
-const ruleTester = new RuleTester();
+var ruleTester = new RuleTester();
 
-ruleTester.run('my-rule', rule, {
+ruleTester.run('convert lodash map to array map', rule, {
   valid: [
     {
       code: 'var _= {map: function(){}}; _.map(collection, fn);',
     },
     {
+      code:
+        'var _ = require("lodash"); _= {map: function(){}}; _.map(collection, fn);',
+    },
+    {
       code: '_.map({ a:1, b:2 }, fn);',
+    },
+    {
+      code: 'function fn(_) { _.map(a, fn); }',
+    },
+    {
+      code: 'Array.isArray(a) ? a.map(fn) : _.map(a, fn)',
     },
   ],
 
   invalid: [
     {
       code: '_.map(collection, fn)',
+      errors: [{ messageId: 'convertToArrayMap' }],
+    },
+    {
+      code:
+        '_ = {map: function(){}}; _ = require("lodash"); _.map(collection, fn);',
+      errors: [{ messageId: 'convertToArrayMap' }],
+    },
+    {
+      code: '_.map(collection, fn); _.map(collection, fn);',
+      errors: [
+        { messageId: 'convertToArrayMap' },
+        { messageId: 'convertToArrayMap' },
+      ],
+    },
+    {
+      code: '_.map(collection, fn); _ = {}; _.map(collection, fn);',
       errors: [{ messageId: 'convertToArrayMap' }],
     },
   ],
